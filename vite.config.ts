de4e63d77d0c4bd1062d7a89d6bd3b5146793ef5
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-04 01:46:33 +0900

    init : path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +14,17 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@api': '/src/api',
+      '@assets': '/src/assets',
+      '@common': '/src/common',
+      '@page': '/src/page',
+      '@route': '/src/route',
+      '@shared': '/src/shared',
+      '@style': '/src/style',
+      '@type': '/src/type',
+    },
+  },
 });
