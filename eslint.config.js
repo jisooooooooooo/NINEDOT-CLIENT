ba82dc99a4ac97d475cee0df69ed82a591349d8d
Saebom <spring180@naver.com>
2025-07-04 16:28:53 +0900

    fix: 키보드 이벤트 설정 해제

--- eslint.config.js
+++ eslint.config.js
@@ -30,11 +30,7 @@ export default tseslint.config({
     },
   },
   ignores: ['node_modules', 'dist', 'dist-ssr', '*.config.js', '*.config.ts'],
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettierConfig,
-  ],
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
   rules: {
     ...reactHooks.configs.recommended.rules,
     ...jsxA11y.configs.recommended.rules,
@@ -55,5 +51,7 @@ export default tseslint.config({
     'react/jsx-pascal-case': 'error',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': 'error',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
   },
 });
