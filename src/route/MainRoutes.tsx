cdb1be4c275c0e1043298ea57937cce39eefabdd
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-05 12:13:25 +0900

    feat: layout 추가

--- src/route/MainRoutes.tsx
+++ src/route/MainRoutes.tsx
@@ -4,32 +4,38 @@ import { PATH } from './path';
 
 import { Home, Todo, Mandal, History } from '@/page';
 import { UpperGoal, LowerGoal } from '@/page/todo';
+import { Layout } from '@/shared/component/Layout';
 
 export const mainRoutes: RouteObject[] = [
   {
-    path: PATH.ROOT,
-    element: <Home />,
-  },
-  {
-    path: PATH.TODO,
-    element: <Todo />,
+    element: <Layout />,
     children: [
       {
-        path: 'upper',
-        element: <UpperGoal />,
+        path: PATH.ROOT,
+        element: <Home />,
       },
       {
-        path: 'lower',
-        element: <LowerGoal />,
+        path: PATH.TODO,
+        element: <Todo />,
+        children: [
+          {
+            path: 'upper',
+            element: <UpperGoal />,
+          },
+          {
+            path: 'lower',
+            element: <LowerGoal />,
+          },
+        ],
+      },
+      {
+        path: PATH.MANDAL,
+        element: <Mandal />,
+      },
+      {
+        path: PATH.HISTORY,
+        element: <History />,
       },
     ],
-  },
-  {
-    path: PATH.MANDAL,
-    element: <Mandal />,
-  },
-  {
-    path: PATH.HISTORY,
-    element: <History />,
   },
 ];
