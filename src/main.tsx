eadd9d3043b7c24fa2c08815f63d0f130947689e
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-05 15:58:26 +0900

    fix: ESLint 자동 포맷팅 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App.tsx';
+
 import './style/global.css.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { queryClient } from './common/util/queryClient.ts';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      {import.meta.env.DEV && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 );
-
