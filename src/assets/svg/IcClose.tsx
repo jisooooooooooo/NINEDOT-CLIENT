a173c918d4e3c7ca6e0ae6ad21bbe2aa5126ae7c
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-07 23:39:53 +0900

    chore: close 아이콘 수정

--- src/assets/svg/IcClose.tsx
+++ src/assets/svg/IcClose.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcClose = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 21" {...props}>
-    <path fill="#1B49D7" d="M0 .5h20v20H0z" />
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
     <path
       stroke="#E3E4E5"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={1.7}
-      d="m5 15.5 10-10m-10 0 10 10"
+      strokeWidth={2}
+      d="M5 19 19 5M5 5l14 14"
     />
   </svg>
 );
