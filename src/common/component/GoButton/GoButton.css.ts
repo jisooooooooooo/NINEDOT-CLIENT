a5e56643f8cfcb50dfecfb28a181ecec836a8866
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-07 22:23:38 +0900

    style: GoButton 커서를 활성화 상태일 때만으로 수정

--- src/common/component/GoButton/GoButton.css.ts
+++ src/common/component/GoButton/GoButton.css.ts
@@ -9,12 +9,12 @@ export const goButtonContainer = recipe({
     padding: '2rem',
     border: 'none',
     borderRadius: '1.2rem',
-    cursor: 'pointer',
   },
   variants: {
     state: {
       active: {
         backgroundImage: colors.gradient04,
+        cursor: 'pointer',
       },
       disabled: {
         backgroundColor: colors.grey4,
