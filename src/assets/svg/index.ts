9f06e10e2c1956ef8da4473e7e19ca77548b0136
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-08 02:45:38 +0900

    chore: close 아이콘 수정

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,8 +1,8 @@
 export { default as IcBigNext } from './IcBigNext';
 export { default as IcCheckboxChecked } from './IcCheckboxChecked';
 export { default as IcCheckboxDefault } from './IcCheckboxDefault';
-export { default as IcClose } from './IcClose';
 export { default as IcDropdown } from './IcDropdown';
 export { default as IcModalDelete } from './IcModalDelete';
+export { default as IcTooltipDelete } from './IcTooltipDelete';
 export { default as IcTriangle } from './IcTriangle';
 export { default as Vite } from './Vite';
