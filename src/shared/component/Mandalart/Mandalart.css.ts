7f00efe8036c5837332e7d6ba49b7790061a7969
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-07 18:55:57 +0900

    fix: gap 수정

--- src/shared/component/Mandalart/Mandalart.css.ts
+++ src/shared/component/Mandalart/Mandalart.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const grid = style({
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
-  gap: '1.9rem',
+  gap: '1.6rem',
   width: 'fit-content',
   margin: '0 auto',
 });
