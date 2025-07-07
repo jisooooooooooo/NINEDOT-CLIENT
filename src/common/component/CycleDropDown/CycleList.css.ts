234ae6588081ea44982d2cb286fb8819c932e595
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-07 19:05:50 +0900

    feat: CycleList 컴포넌트 제작

---
+++ src/common/component/CycleDropDown/CycleList.css.ts
@@ -0,0 +1,50 @@
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+import { colors, fonts } from '@/style/token';
+
+export const listContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '13.2rem',
+  backgroundColor: colors.grey2,
+  borderRadius: '8px',
+  cursor: 'pointer',
+  overflow: 'hidden',
+});
+
+export const listItem = style({
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  height: '5.6rem',
+  padding: '1.4em 2rem',
+  borderBottom: `solid 1px ${colors.grey3}`,
+
+  selectors: {
+    '&:last-child': {
+      borderBottom: 'none',
+    },
+    '&:hover': {
+      backgroundColor: colors.grey1,
+    },
+  },
+});
+
+export const listText = recipe({
+  variants: {
+    state: {
+      selected: {
+        ...fonts.subtitle01,
+        color: colors.grey10,
+      },
+      default: {
+        ...fonts.subtitle03,
+        color: '#58595D',
+      },
+    },
+    defaultVariants: {
+      state: 'default',
+    },
+  },
+});
---
+++ src/common/component/CycleDropDown/CycleList.tsx
@@ -0,0 +1,30 @@
+import { listContainer, listItem, listText } from '@/common/component/CycleDropDown/CycleList.css';
+import { useState } from 'react';
+
+const CYCLE_TYPE = ['매일', '매주', '한 번'] as const;
+
+type CycleType = (typeof CYCLE_TYPE)[number];
+
+const CycleList = () => {
+  const [selectedType, setSelectedType] = useState<CycleType>(CYCLE_TYPE[0]);
+
+  const handleType = (currentType: CycleType) => {
+    setSelectedType(currentType);
+  };
+
+  return (
+    <div className={listContainer}>
+      {CYCLE_TYPE.map((cycle) => {
+        const state = selectedType === cycle ? 'selected' : 'default';
+
+        return (
+          <button key={cycle} className={listItem} onClick={() => handleType(cycle)}>
+            <p className={listText({ state })}>{cycle}</p>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CycleList;
