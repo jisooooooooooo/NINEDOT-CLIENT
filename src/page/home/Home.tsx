0953f3067d7ff30eb75d3cbd4a5421b55ea2914a
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-08 15:37:22 +0900

    refactor: 홈 페이지 테스트 코드 정리

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,18 +1,7 @@
-import { useModal } from '@/common/hook/useModal';
-import AiRecommendModal from '@/common/component/AiRecommendModal/AiRecommendModal';
-import Tooltip from '@/common/component/Tooltip/Tooltip';
-
 const Home = () => {
-  const { openModal, ModalWrapper, closeModal } = useModal();
-
   return (
     <div>
       <h1>홈</h1>
-      <Tooltip />
-      <button onClick={() => openModal(<AiRecommendModal onClose={closeModal} />)}>
-        AI 추천 모달 열기
-      </button>
-      {ModalWrapper}
     </div>
   );
 };
