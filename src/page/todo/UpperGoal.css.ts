0bb900914c6cfc59b135a296b617b09d256aff20
임지수 <dlawltn0708@naver.com>
2025-07-08 11:26:53 +0900

    fix: 레이아웃 스크롤 기능 활성화

--- src/page/todo/UpperGoal.css.ts
+++ src/page/todo/UpperGoal.css.ts
@@ -1,9 +1,10 @@
 import { style } from '@vanilla-extract/css';
 
 import { colors } from '@/style/token';
+import { zIndex } from '@/style/token';
 
 export const upperGoalContainer = style({
-  height: '100vh',
+  height: '100%',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
@@ -11,4 +12,17 @@ export const upperGoalContainer = style({
   backgroundColor: colors.bg_black01,
   position: 'relative',
   overflow: 'hidden',
+});
+
+export const upperGoalBox = style({
+  width: '128rem',
+  height: '67.2rem',
+  flexShrink: 0,
+  borderRadius: '12px',
+  background: 'rgba(65, 69, 76, 0.32)',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  zIndex: Number(zIndex.modal),
 });
--- src/shared/component/Layout/Layout.css.ts
+++ src/shared/component/Layout/Layout.css.ts
@@ -4,10 +4,8 @@ export const layoutContainer = style({
   display: 'flex',
   flexDirection: 'column',
   height: '100vh',
-  overflow: 'hidden',
 });
 
 export const layoutMain = style({
   flex: 1,
-  overflow: 'hidden',
 });
