1aaee7be5ece255afd2b80d020bbd74b7ae19a32
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-08 23:51:44 +0900

    refactor: Home 페이지 테스트 코드 제거

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,37 +1,7 @@
-import { useState } from 'react';
-import MandalartTextField from '@/common/component/MandalartTextField';
-
 const Home = () => {
-  const [bigGoal, setBigGoal] = useState('');
-  const [subGoal, setSubGoal] = useState('');
-  const [todo, setTodo] = useState('');
-
   return (
-    <div style={{ padding: '2rem' }}>
-      <div style={{ marginBottom: '2rem' }}>
-        <div style={{ marginBottom: '0.5rem', fontWeight: 600 }}>상위목표(bigGoal)</div>
-        <MandalartTextField
-          variant="bigGoal"
-          value={bigGoal}
-          onChange={setBigGoal}
-        />
-      </div>
-      <div style={{ marginBottom: '2rem' }}>
-        <div style={{ marginBottom: '0.5rem', fontWeight: 600 }}>하위목표(subGoal)</div>
-        <MandalartTextField
-          variant="subGoal"
-          value={subGoal}
-          onChange={setSubGoal}
-        />
-      </div>
-      <div>
-        <div style={{ marginBottom: '0.5rem', fontWeight: 600 }}>할 일(todo)</div>
-        <MandalartTextField
-          variant="todo"
-          value={todo}
-          onChange={setTodo}
-        />
-      </div>
+    <div>
+      <h1>홈</h1>
     </div>
   );
 };
