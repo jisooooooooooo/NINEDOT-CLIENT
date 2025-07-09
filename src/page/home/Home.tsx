3a9fe0bce43e31c3fc220fae55a2268a1ffc6a6c
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-10 04:44:46 +0900

    test: 회원가입 텍스트 필드 테스트 코드 제거

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,44 +1,7 @@
-import { useState } from 'react';
-
-import SignupTextField from '@/common/component/SignupTextField';
-import { MOCK_SIGNUP_DATA } from '@/common/component/SignupTextField/SignupTextField.mock';
-
 const Home = () => {
-  // mock 데이터로 초기값 설정 (구글 계정에서 받아온 것처럼)
-  const [name, setName] = useState(MOCK_SIGNUP_DATA.name);
-  const [email, setEmail] = useState(MOCK_SIGNUP_DATA.email);
-  const [birth, setBirth] = useState(MOCK_SIGNUP_DATA.birth);
-  const [job, setJob] = useState('');
-
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-        width: '52.2rem',
-        margin: '2rem auto',
-      }}
-    >
-      <SignupTextField
-        type="name"
-        value={name}
-        onChange={setName}
-        placeholder="이름을 입력해주세요"
-      />
-      <SignupTextField
-        type="email"
-        value={email}
-        onChange={setEmail}
-        disabled // 이메일은 잠금 상태 (수정 불가)
-      />
-      <SignupTextField
-        type="birth"
-        value={birth}
-        onChange={setBirth}
-        placeholder="생년월일을 입력해주세요"
-      />
-      <SignupTextField type="job" value={job} onChange={setJob} placeholder="정보를 입력해주세요" />
+    <div>
+      <h1>홈</h1>
     </div>
   );
 };
