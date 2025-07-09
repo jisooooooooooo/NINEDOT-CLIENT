33c1f1a1ada3f838fa6ac8759eea59315fb91aa7
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-10 01:45:36 +0900

    delete: 테스트 코드 삭제

--- src/page/todo/UpperGoal.tsx
+++ src/page/todo/UpperGoal.tsx
@@ -1,16 +1,7 @@
-import Mandalart from '@/common/component/Mandalart/Mandalart';
-import * as styles from './Todo.css';
-
 const UpperGoal = () => {
   return (
     <div>
       <h2>상위 목표 설정</h2>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}>
-        <Mandalart type="TODO_SUB" />
-        <Mandalart type="TODO_MAIN" />
-        <Mandalart type="TODO_EDIT" />
-        <Mandalart type="MY_MANDAL" />
-      </div>
       {/* TODO: 상위 목표 입력 폼 */}
     </div>
   );
