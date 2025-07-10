444538bc06632234ed0d6fdb80b38ba6c5888af0
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-10 21:45:27 +0900

    feat: 스타일 수정

--- src/page/mandal/component/EditBtn/EditBtn.css.ts
+++ src/page/mandal/component/EditBtn/EditBtn.css.ts
@@ -4,23 +4,23 @@ import { colors } from '@/style/token';
 import { fonts } from '@/style/token/typography.css';
 
 export const editBtnWrapper = style({
+  position: 'relative',
   display: 'flex',
-  width: '6.4rem',
+  width: '17.5rem',
   height: '6.4rem',
-  justifyContent: 'center',
+  padding: '0',
+  justifyContent: 'flex-end',
   alignItems: 'center',
   gap: '1rem',
   flexShrink: 0,
-  borderRadius: '50%',
+  borderRadius: '50px',
   backgroundColor: colors.grey3,
   cursor: 'pointer',
-  transition: 'all 0.3s ease',
+  clipPath: 'circle(3.2rem at 14.3rem 3.2rem)',
+  transition: 'clip-path 0.3s ease',
   ':hover': {
-    width: '17.5rem',
-    padding: '0 0 0 2.4rem',
-    justifyContent: 'space-between',
-    borderRadius: '50px',
     backgroundImage: colors.gradient05,
+    clipPath: 'circle(100% at center)',
   },
 });
 
@@ -30,24 +30,30 @@ export const iconSvg = style({
 });
 
 export const editIcon = style({
+  position: 'absolute',
+  right: 0,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  width: '6.4rem',
+  height: '6.4rem',
   borderRadius: '50%',
   backgroundColor: colors.grey3,
   color: colors.white01,
+  zIndex: 1,
 });
 
 export const editText = style([
   fonts.subtitle05,
   {
-    display: 'none',
+    position: 'absolute',
+    left: '2.4rem',
+    visibility: 'hidden',
     color: colors.white01,
-  },
-  {
+    whiteSpace: 'nowrap',
     selectors: {
       [`${editBtnWrapper}:hover &`]: {
-        display: 'block',
+        visibility: 'visible',
       },
     },
   },
