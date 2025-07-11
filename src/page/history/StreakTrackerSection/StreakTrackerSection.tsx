c37e79abaea4475d93555d3552e679172710440f
Saebom <spring180@naver.com>
2025-07-12 03:34:37 +0900

    feat: 날짜 클릭하면 detail 내용 변하도록 기능 수정

--- src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
+++ src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
@@ -3,13 +3,16 @@ import StreakGrid from '@/page/history/component/StreakGrid/StreakGrid';
 import { dayData } from '@/page/history/sampleData/dayData';
 import { todoData } from '@/page/history/sampleData/todoData';
 import * as styles from '@/page/history/StreakTrackerSection/StreakTrackerSection.css';
+import { useState } from 'react';
 
 const StreakTracker = () => {
-  const detailData = todoData.streaks.find((streak) => streak.streakDay === dayData.progressDays);
+  const [selectedDay, setSelectedDay] = useState(dayData.progressDays);
+
+  const detailData = todoData.streaks.find((streak) => streak.streakDay === selectedDay);
 
   return (
     <div className={styles.streakTrackerContainer}>
-      <StreakGrid progressDays={dayData.progressDays} />
+      <StreakGrid progressDays={dayData.progressDays} onClick={setSelectedDay} />
       <StreakDetail detailData={detailData} />
     </div>
   );
--- src/page/history/component/StreakGrid/StreakGrid.tsx
+++ src/page/history/component/StreakGrid/StreakGrid.tsx
@@ -5,14 +5,22 @@ const TOTAL_DOTS = 66;
 
 type StreakGridProps = {
   progressDays: number;
+
+  onClick: (day: number) => void;
 };
 
-const StreakGrid = ({ progressDays }: StreakGridProps) => {
+const StreakGrid = ({ progressDays, onClick }: StreakGridProps) => {
   const dots = Array.from({ length: TOTAL_DOTS }, (_, i) => {
     const isFilled = i < progressDays;
+    const day = i + 1;
+
     const DotIcon = isFilled ? IcStreakerDot : IcStreakerDotDefault;
 
-    return <DotIcon key={i} className={styles.dotIcon({ clickable: isFilled })} />;
+    return (
+      <button onClick={isFilled ? () => onClick(day) : undefined}>
+        <DotIcon key={i} className={styles.dotIcon({ clickable: isFilled })} />
+      </button>
+    );
   });
 
   return <div className={styles.gridContainer}>{dots}</div>;
