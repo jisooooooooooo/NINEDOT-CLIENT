1cf28a31bb32fdd275b7520dac6bbfd75e694ca0
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-12 01:16:49 +0900

    style: TodoBox 스타일 수정

--- src/page/todo/myTodo/component/TodoBox/TodoBox.css.ts
+++ src/page/todo/myTodo/component/TodoBox/TodoBox.css.ts
@@ -1,8 +1,7 @@
 import { style } from '@vanilla-extract/css';
-
 import { colors } from '@/style/token/color.css';
+import { fonts } from '@/style/token/typography.css';
 
-// Recommend 컨테이너
 export const recommendContainer = style({
   display: 'flex',
   width: '40.6rem',
@@ -15,16 +14,13 @@ export const recommendContainer = style({
   background: colors.grey4,
 });
 
-// Recommend 아이템
 export const recommendItem = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   alignSelf: 'stretch',
-  // border: `1px solid ${colors.grey5}`,
 });
 
-// Recommend 텍스트
 export const recommendText = style({
   display: '-webkit-box',
   width: '33.3rem',
@@ -34,13 +30,10 @@ export const recommendText = style({
   color: colors.white01,
   textOverflow: 'ellipsis',
   fontFamily: 'Pretendard',
-  fontSize: '1.8rem',
   fontStyle: 'normal',
-  fontWeight: 700,
-  lineHeight: '140%',
+  ...fonts.subtitle05,
 });
 
-// Todo 컨테이너
 export const todoContainer = style({
   display: 'flex',
   width: '43.6rem',
@@ -50,27 +43,21 @@ export const todoContainer = style({
   background: colors.grey4,
 });
 
-// Todo 아이템
 export const todoItem = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   flex: '1 0 0',
-  // border: `1px solid ${colors.grey5}`,
 });
 
-// Todo 텍스트
 export const todoText = style({
   color: colors.grey10,
   textAlign: 'center',
   fontFamily: 'Pretendard',
-  fontSize: '2rem',
   fontStyle: 'normal',
-  fontWeight: 600,
-  lineHeight: '140%',
+  ...fonts.subtitle02,
 });
 
-// 체크박스 버튼(공통)
 export const checkboxButton = style({
   display: 'flex',
   alignItems: 'center',
@@ -84,7 +71,6 @@ export const checkboxButton = style({
   cursor: 'pointer',
 });
 
-// 체크박스 아이콘(공통)
 export const checkboxIcon = style({
   width: '2.4rem',
   height: '2.4rem',
