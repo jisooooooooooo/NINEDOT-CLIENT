43747e1f36ac7873ea4668381f84839c3302971e
Saebom <spring180@naver.com>
2025-07-11 23:57:55 +0900

    chore: streak tracker 아이콘 추가

---
+++ src/assets/svg/IcStreakerDotDefault.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcStreakerDotDefault = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 36 36" {...props}>
+    <rect width={36} height={36} fill="#282C33" rx={18} />
+  </svg>
+);
+export default SvgIcStreakerDotDefault;
