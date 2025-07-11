3e6c8ec6f85119eda7b0ef7cbd20c42767888a39
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-12 01:07:02 +0900

    feat: TodoBox 컴포넌트 구현

---
+++ src/page/todo/myTodo/component/TodoBox.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import type { TodoBoxProps, TodoItemTypes } from './TodoBox.types';
+
+import {
+  recommendContainer,
+  recommendItem,
+  recommendText,
+  todoContainer,
+  todoItem,
+  todoText,
+  checkboxButton,
+  checkboxIcon,
+} from './TodoBox.css';
+import { IcCheckboxChecked, IcCheckboxDefault } from '@/assets/svg';
+
+interface Props extends TodoBoxProps {
+  type: 'recommend' | 'todo';
+}
+
+const TodoItem: React.FC<{
+  item: TodoItemTypes;
+  type: 'recommend' | 'todo';
+  onItemClick?: (item: TodoItemTypes) => void;
+}> = ({ item, type, onItemClick }) => {
+  const handleClick = () => {
+    onItemClick?.(item);
+  };
+  return (
+    <div className={type === 'recommend' ? recommendItem : todoItem}>
+      <span className={type === 'recommend' ? recommendText : todoText}>{item.content}</span>
+      <button
+        className={checkboxButton}
+        onClick={handleClick}
+        type="button"
+        aria-label={item.completed ? '완료 취소하기' : '완료하기'}
+      >
+        {item.completed ? (
+          <IcCheckboxChecked className={checkboxIcon} />
+        ) : (
+          <IcCheckboxDefault className={checkboxIcon} />
+        )}
+      </button>
+    </div>
+  );
+};
+
+const TodoBox: React.FC<Props> = ({ type, items, onItemClick, className }) => {
+  const containerClass = type === 'recommend' ? recommendContainer : todoContainer;
+  return (
+    <div className={`${containerClass} ${className || ''}`}>
+      {items.map((item) => (
+        <TodoItem key={item.id} item={item} type={type} onItemClick={onItemClick} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoBox;
---
+++ src/page/todo/myTodo/component/index.ts
@@ -0,0 +1,2 @@
+export { default as TodoBox } from './TodoBox';
+export type { TodoBoxProps, TodoItemTypes, TodoModeTypes } from './TodoBox.types';
