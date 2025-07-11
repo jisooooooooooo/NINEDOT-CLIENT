7fa6010d07bc5f36d4bc8c7745b3f0d088060039
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-12 08:55:21 +0900

    test: 테스트 코드 제거

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,33 +1,7 @@
-import { useState } from 'react';
-
-import { DatePicker } from '../todo/myTodo/component/DatePicker';
-
-import { createDate } from '@/common/util/format';
-
-const MIN_DATE = createDate(2025, 7, 10);
-const MAX_DATE = createDate(2025, 7, 20);
-
 const Home = () => {
-  const [currentDate, setCurrentDate] = useState(createDate(2025, 7, 15));
-
-  const hasPrev = currentDate > MIN_DATE;
-  const hasNext = currentDate < MAX_DATE;
-
-  const handleDateChange = (newDate: Date) => {
-    setCurrentDate(newDate);
-  };
-
   return (
     <div>
       <h1>홈</h1>
-      <div style={{ padding: '2rem' }}>
-        <DatePicker
-          currentDate={currentDate}
-          onDateChange={handleDateChange}
-          hasPrev={hasPrev}
-          hasNext={hasNext}
-        />
-      </div>
     </div>
   );
 };
