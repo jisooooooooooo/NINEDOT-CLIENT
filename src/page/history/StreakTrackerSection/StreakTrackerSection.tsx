4c60775aa39d4340c46e76aed9469a5613c8a619
Saebom <spring180@naver.com>
2025-07-12 01:59:25 +0900

    feat: StreakDetail 컴포넌트로 props 넘겨주기

--- src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
+++ src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
@@ -1,13 +1,16 @@
 import StreakDetail from '@/page/history/component/StreakDetail/StreakDetail';
 import StreakGrid from '@/page/history/component/StreakGrid/StreakGrid';
 import { dayData } from '@/page/history/sampleData/dayData';
+import { todoData } from '@/page/history/sampleData/todoData';
 import * as styles from '@/page/history/StreakTrackerSection/StreakTrackerSection.css';
 
 const StreakTracker = () => {
+  const detailData = todoData.streaks.find((streak) => streak.streakDay === dayData.progressDays);
+
   return (
     <div className={styles.streakTrackerContainer}>
       <StreakGrid progressDays={dayData.progressDays} />
-      <StreakDetail />
+      <StreakDetail detailData={detailData} />
     </div>
   );
 };
