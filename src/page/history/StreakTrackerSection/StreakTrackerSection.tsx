81be7ee138c9f8a2baa6f7759b1da4f10fb8ca74
Saebom <spring180@naver.com>
2025-07-12 16:21:18 +0900

    refactor: 빌드 에러 해결

--- src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
+++ src/page/history/StreakTrackerSection/StreakTrackerSection.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
+
 import StreakDetail from '@/page/history/component/StreakDetail/StreakDetail';
 import StreakGrid from '@/page/history/component/StreakGrid/StreakGrid';
 import { dayData } from '@/page/history/sampleData/dayData';
 import { todoData } from '@/page/history/sampleData/todoData';
 import * as styles from '@/page/history/StreakTrackerSection/StreakTrackerSection.css';
-import { useState } from 'react';
 
 const StreakTracker = () => {
   const [selectedDay, setSelectedDay] = useState(dayData.progressDays);
