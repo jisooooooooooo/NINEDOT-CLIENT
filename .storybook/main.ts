cf783b0f2989c379ebaaff49a3dc5e9df0575dd0
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-12 23:37:38 +0900

    fix: storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,16 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import svgr from 'vite-plugin-svgr';
-import type { UserConfig } from 'vite';
 
-// Chromatic deployment configuration
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
-  },
-  viteFinal: (config: UserConfig) => {
-    config.plugins = config.plugins || [];
-    config.plugins.push(vanillaExtractPlugin());
-    config.plugins.push(svgr());
-
-    config.optimizeDeps = {
-      ...(config.optimizeDeps || {}),
-      include: ['@vanilla-extract/css'],
-    };
-
-    config.build = {
-      ...(config.build || {}),
-      target: 'esnext',
-      minify: false,
-    };
-
-    return config;
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,34 +1,14 @@
 import type { Preview } from '@storybook/react';
-import '../src/style/reset.css';
-import '../src/style/global.css';
+import '../src/shared/styles/global.css';
+import '../src/shared/styles/reset.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
-    },
-    backgrounds: {
-      default: 'dark',
-      values: [
-        {
-          name: 'dark',
-          value: '#02050B',
-        },
-        {
-          name: 'light',
-          value: '#ffffff',
-        },
-      ],
-    },
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo',
     },
   },
 };
