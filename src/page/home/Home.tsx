7dea321e1d65db474b38e149b949ab12ea4f54cd
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-15 19:36:27 +0900

    feat: Intro 컴포넌트 생성

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,35 +1,5 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import * as styles from '@/page/home/Home.css';
-import UserModal from '@/common/component/UserModal/UserModal';
-
-const START_MESSAGE = '목표를 이루는 66일의 여정 <br/> 함께 시작해볼까요?';
-const CONTINUE_MESSAGE = '작성하던 만다라트가 있어요 <br/> 마저 목표를 세워볼까요?';
-
-const START_BTN_MESSAGE = '만다라트 만들기';
-const CONTINUE_BTN_MESSAGE = '이어서 작성하기';
-
 const Home = () => {
-  const isWritten = true;
-
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate('/todo');
-  };
-
-  return (
-    <main className={styles.homeWrapper}>
-      <h1
-        className={styles.homeText}
-        dangerouslySetInnerHTML={{ __html: isWritten ? CONTINUE_MESSAGE : START_MESSAGE }}
-      />
-      <button className={styles.buttonContainer} onClick={handleClick}>
-        {isWritten ? CONTINUE_BTN_MESSAGE : START_BTN_MESSAGE}
-      </button>
-    </main>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
--- src/page/intro/Intro.tsx
+++ src/page/intro/Intro.tsx
@@ -1,5 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+import * as styles from '@/page/intro/Intro.css';
+
+const MESSAGE = {
+  START: {
+    title: '목표를 이루는 66일의 여정 <br/> 함께 시작해볼까요?',
+    button: '만다라트 만들기',
+  },
+  CONTINUE: {
+    title: '작성하던 만다라트가 있어요 <br/> 마저 목표를 세워볼까요?',
+    button: '이어서 작성하기',
+  },
+};
+
 const Intro = () => {
-  return <div>intro</div>;
+  const isWritten = true;
+  const navigate = useNavigate();
+
+  const handleNavigateToTodo = () => {
+    navigate('/todo');
+  };
+
+  const content = isWritten ? MESSAGE.CONTINUE : MESSAGE.START;
+
+  return (
+    <main className={styles.introContainer}>
+      <h1 className={styles.introText} dangerouslySetInnerHTML={{ __html: content.title }} />
+      <button className={styles.buttonContainer} onClick={handleNavigateToTodo}>
+        {content.button}
+      </button>
+    </main>
+  );
 };
 
 export default Intro;
