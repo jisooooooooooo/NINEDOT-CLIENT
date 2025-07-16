6d89b51ca386103998260829b010c00737d59d8f
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-17 04:19:37 +0900

    feat: Streak Tracker에 데이터 연결

--- src/page/history/component/StreakGrid/StreakGrid.tsx
+++ src/page/history/component/StreakGrid/StreakGrid.tsx
@@ -1,8 +1,10 @@
+import { useGetStreak } from '@/api/domain/history/hook/useGetStreak';
 import { IcStreakerDot, IcStreakerDotDefault } from '@/assets/svg';
+import Loading from '@/common/component/Loading/Loading';
 import * as styles from '@/page/history/component/StreakGrid/StreakGrid.css';
-import { todoData } from '@/page/history/sampleData/todoData';
 
 const TOTAL_DOTS = 66;
+const MANDALART_ID = 1;
 
 type StreakGridProps = {
   onHover: (day: number | null) => void;
@@ -10,10 +12,16 @@ type StreakGridProps = {
 };
 
 const StreakGrid = ({ onHover, onSelect }: StreakGridProps) => {
+  const { data, isLoading } = useGetStreak(MANDALART_ID);
+
+  if (isLoading || !data) {
+    return <Loading />;
+  }
+
   const dots = Array.from({ length: TOTAL_DOTS }, (_, i) => {
     const day = i + 1;
 
-    const streak = todoData.streaks.find((s) => s.streakDay === day);
+    const streak = data.streaks.find((s) => s.streakDay === day);
     const isFilled = streak !== undefined && streak.completedTodoCount > 0;
 
     const DotIcon = isFilled ? IcStreakerDot : IcStreakerDotDefault;
