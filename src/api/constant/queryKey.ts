c81ca01f3385a0652a3efe3dbf4ebe4d605213e3
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-17 03:47:57 +0900

    feat: 쿼리 키 추가

--- src/api/constant/queryKey.ts
+++ src/api/constant/queryKey.ts
@@ -1,4 +1,5 @@
 export const QUERY_KEY = {
   OVERALL_TODO: ['overallTodo'],
   HISTORY: (mandalartId: number) => ['mandalartHistoryList', mandalartId],
+  STREAKS: (mandalartId: number) => ['streaks', mandalartId],
 } as const;
