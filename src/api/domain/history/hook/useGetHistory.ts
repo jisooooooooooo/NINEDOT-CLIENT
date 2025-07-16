cc6ed72b080cc5ab788fa18c2e0c7a2eece2e222
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-17 03:17:51 +0900

    feat: 히스토리 응답값 수정

--- src/api/domain/history/hook/useGetHistory.ts
+++ src/api/domain/history/hook/useGetHistory.ts
@@ -2,11 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getHistory } from '@/api/domain/history';
 import { QUERY_KEY } from '@/api/constant/queryKey';
-import type { BaseResponse } from '@/type/api';
 import type { historyResponse } from '@/api/domain/history/type/historyResponse';
 
 export const useGetHistory = (mandalartId: number) => {
-  return useQuery<BaseResponse<historyResponse>>({
+  return useQuery<historyResponse>({
     queryKey: QUERY_KEY.HISTORY(mandalartId),
     queryFn: () => getHistory(mandalartId),
   });
