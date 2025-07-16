1668bfb1a6fdc36da5bcee1454582444a8f351ac
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-17 03:15:43 +0900

    feat: 엔드포인트 추가 & 반환값 수정

--- src/api/domain/history/index.ts
+++ src/api/domain/history/index.ts
@@ -1,10 +1,11 @@
 import axiosInstance from '@/api/axiosInstance';
+import { END_POINT } from '@/api/constant/endPoint';
 import type { historyResponse } from '@/api/domain/history/type/historyResponse';
 import type { BaseResponse } from '@/type/api';
 
 export const getHistory = async (mandalartId: number) => {
   const { data } = await axiosInstance.get<BaseResponse<historyResponse>>(
-    `/mandalarts/${mandalartId}/histories`,
+    `/${END_POINT.MANDALART}/${mandalartId}/histories`,
   );
-  return data;
+  return data.data;
 };
