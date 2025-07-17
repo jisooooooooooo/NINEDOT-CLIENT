c0f206be4ce5579cae363a73a303ab8f0b9d178d
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-18 03:13:58 +0900

    feat: 로딩 컴포넌트 제작

--- src/common/component/Loading/Loading.tsx
+++ src/common/component/Loading/Loading.tsx
@@ -1,5 +1,23 @@
-const Loading = () => {
-  return <div>로딩중</div>;
+import Lottie from 'lottie-react';
+
+import loadingAnimation from '@/assets/lottie/loading.json';
+import * as styles from '@/common/component/Loading/Loading.css';
+
+type LoadingProps = {
+  type: 'goal' | 'todo';
+};
+
+const Loading = ({ type }: LoadingProps) => {
+  const typeText = type === 'goal' ? '목표' : '할 일';
+
+  return (
+    <div className={styles.loadingOverlay}>
+      <div className={styles.loadingContainer}>
+        <Lottie className={styles.loadingLottie} animationData={loadingAnimation} />
+        <p className={styles.loadingText}>AI가 {typeText}을 추천해주고 있어요</p>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
