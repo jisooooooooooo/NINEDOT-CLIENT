7b11d6db6fd57644a3fa0f7b0f08bfeeee0bd4a0
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-18 19:21:56 +0900

    feat: 리디렉트 uri 주소 변경

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -3,8 +3,9 @@ import axiosInstance from '@/api/axiosInstance';
 const getAccessToken = async (code: string) => {
   const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
 
+  console.log(redirectUri);
   const response = await axiosInstance.post(
-    `/auth/oauth2/google/callback?redirect_uri=${encodeURIComponent(redirectUri)}`,
+    `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
     { code },
   );
 
