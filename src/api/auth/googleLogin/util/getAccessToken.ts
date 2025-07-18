8f9ff7c8602e90dd18e528686c909bce207b9968
봄 <saebom@Saebomui-MacBookPro.local>
2025-07-19 02:03:01 +0900

    feat: 구글 로그인 연결

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,12 +1,9 @@
 import axiosInstance from '@/api/axiosInstance';
 
 const getAccessToken = async (code: string) => {
-  const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
+  // const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
 
-  const response = await axiosInstance.post(
-    `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
-    { code },
-  );
+  const response = await axiosInstance.post(`/auth/oauth2/google/callback`, { code });
 
   return response.data.data;
 };
