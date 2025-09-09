9b1a00d6e9a84b5a903e1dfa3c6ef718d1291a97
Saebom <spring180@naver.com>
2025-09-09 22:52:59 +0900

    feat: redirect_local로 변경

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,12 +1,13 @@
 import axiosInstance from '@/api/axiosInstance';
 
 const getAccessToken = async (code: string) => {
-  const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
+  const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
 
   const response = await axiosInstance.post(
     `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
     { code },
   );
+
   return response.data.data;
 };
 
