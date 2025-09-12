4c11908a7a5418bc96d6f67efb6a43536afd08ab
Saebom <spring180@naver.com>
2025-09-13 02:39:37 +0900

    feat: userModal 컴포넌트에 user 상태 적용

--- src/common/component/UserModal/UserModal.tsx
+++ src/common/component/UserModal/UserModal.tsx
@@ -2,8 +2,10 @@ import { useEffect, useRef } from 'react';
 
 import { IcDivider } from '@/assets/svg';
 import * as styles from '@/common/component/UserModal/UserModal.css';
-import { useGetUser } from '@/api/domain/signup/hook/useGetUser';
 import { usePostLogout } from '@/api/domain/signup/hook/usePostLogout';
+import { useAuthStore } from '@/store/useAuthStore';
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '@/route';
 
 interface UserModalProps {
   onClose: () => void;
@@ -11,7 +13,11 @@ interface UserModalProps {
 
 const UserModal = ({ onClose }: UserModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
-  const { data: user, isLoading, isError } = useGetUser();
+  const navigate = useNavigate();
+
+  const user = useAuthStore((state) => state.user);
+  const resetUser = useAuthStore((state) => state.resetUser);
+
   const { mutate: logoutMutate } = usePostLogout();
 
   const handleClickOutside = (e: MouseEvent) => {
@@ -24,8 +30,9 @@ const UserModal = ({ onClose }: UserModalProps) => {
     logoutMutate(undefined, {
       onSuccess: () => {
         localStorage.removeItem('accessToken');
+        resetUser();
         onClose();
-        window.location.reload();
+        navigate(PATH.ROOT);
       },
       onError: (error) => {
         console.error('로그아웃 실패:', error);
@@ -41,7 +48,7 @@ const UserModal = ({ onClose }: UserModalProps) => {
     };
   }, []);
 
-  if (isLoading || isError || !user) {
+  if (!user) {
     return null;
   }
 
