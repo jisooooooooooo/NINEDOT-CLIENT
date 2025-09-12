b0965f97bedfe9e8373428ba67a3b44c03d88284
Saebom <spring180@naver.com>
2025-09-12 18:34:55 +0900

    refactor: redirect_uri constant 사용

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,10 +1,9 @@
+import { GOOGLE_REDIRECT_URI } from '@/api/auth/googleLogin/authConstants';
 import axiosInstance from '@/api/axiosInstance';
 
 const getAccessToken = async (code: string) => {
-  const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
-
   const response = await axiosInstance.post(
-    `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
+    `/auth/oauth2/google/callback?redirect_uri=${GOOGLE_REDIRECT_URI}`,
     { code },
   );
 
