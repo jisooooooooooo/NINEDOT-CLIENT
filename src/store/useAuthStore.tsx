3969e9f2f87be76ce03dc955a59e8057596b39e3
Saebom <spring180@naver.com>
2025-09-13 01:05:07 +0900

    feat: zuestand 기반 useAuthStore 구현

---
+++ src/store/useAuthStore.tsx
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+import type { AuthStoreType, UserType } from '@/store/types/authTypes';
+
+const defaultUser: UserType = {
+  name: '',
+  email: '',
+  birthday: '',
+  job: '',
+  profileImageUrl: '',
+  answers: [],
+};
+
+export const useAuthStore = create<AuthStoreType>()(
+  persist(
+    (set) => ({
+      user: defaultUser,
+      setUser: (newUser) => set({ user: newUser }),
+      reset: () => set({ user: defaultUser }),
+    }),
+    {
+      name: 'auth-storage',
+    },
+  ),
+);
