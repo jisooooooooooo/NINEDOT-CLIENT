c555aa1097484ad89beee405974dd2cfebcd7fe4
Jisu Shin <sjsz0811@hanyang.ac.kr>
2025-09-16 21:35:07 +0900

    fix: HoverContent.tsx와 동일하게 ModifyTextfield 상수 변경

--- src/common/component/TextField/modify/constants.ts
+++ src/common/component/TextField/modify/constants.ts
@@ -1,6 +1,6 @@
 export type ModifyVariant = 'subGoal' | 'todo';
 
 export const DEFAULT_PLACEHOLDER = {
-  subGoal: '세부 목표를 입력해주세요',
-  todo: '할 일을 입력해주세요',
+  subGoal: '수정할 목표를 입력해주세요.',
+  todo: '목표를 입력해주세요.',
 } as const;
