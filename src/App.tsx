6c62a96d9e2e920eaca3dc8fbc7839f01292ce84
Saebom <spring180@naver.com>
2025-09-20 23:33:05 +0900

    refactor: lazy 활용해서 코드 분할 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/route';
+import Loading from '@/common/component/Loading/Loading';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<Loading type="goal" />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
