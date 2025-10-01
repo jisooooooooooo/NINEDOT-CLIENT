6f31bb135adfa4cfc9d7a5aaa012da6bf6e0fb68
Saebom <spring180@naver.com>
2025-10-01 13:19:18 +0900

    fix: redirect uri 변경

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,9 +1,17 @@
-import { GOOGLE_REDIRECT_URI } from '@/api/auth/googleLogin/authConstants';
+import {
+  GOOGLE_REDIRECT_URI,
+  GOOGLE_REDIRECT_URI_LOCAL,
+} from '@/api/auth/googleLogin/authConstants';
 import axiosInstance from '@/api/axiosInstance';
 
 const getAccessToken = async (code: string) => {
+  const isLocal =
+    window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+  const redirectUri = isLocal ? GOOGLE_REDIRECT_URI_LOCAL : GOOGLE_REDIRECT_URI;
+
   const response = await axiosInstance.post(
-    `/auth/oauth2/google/callback?redirect_uri=${GOOGLE_REDIRECT_URI}`,
+    `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
     { code },
   );
 
